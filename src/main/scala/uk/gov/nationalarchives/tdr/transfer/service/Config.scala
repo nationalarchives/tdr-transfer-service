30fee43a2d5e5af3c6a57daace0b7d19455df6f7
TomJKing <tom.king@nationalarchives.gov.uk>
2024-06-12 11:34:19 +0100

    Use config for api port

--- src/main/scala/uk/gov/nationalarchives/tdr/transfer/service/Config.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/transfer/service/Config.scala
@@ -1,7 +1,8 @@
 package uk.gov.nationalarchives.tdr.transfer.service
 
 object Config {
+  case class Api(port: Int)
   case class Auth(url: String, realm: String)
 
-  case class Configuration(auth: Auth)
+  case class Configuration(auth: Auth, api: Api)
 }
--- src/main/scala/uk/gov/nationalarchives/tdr/transfer/service/api/TransferServiceServer.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/transfer/service/api/TransferServiceServer.scala
@@ -2,14 +2,22 @@ package uk.gov.nationalarchives.tdr.transfer.service.api
 
 import cats.data.Kleisli
 import cats.effect.{ExitCode, IO, IOApp}
-import com.comcast.ip4s.IpLiteralSyntax
+import com.comcast.ip4s.{IpLiteralSyntax, Port}
 import org.http4s.dsl.io._
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.server.Router
 import org.http4s.server.middleware.Logger
 import org.http4s.{HttpRoutes, Request, Response}
+import pureconfig.ConfigSource
+import pureconfig.generic.auto._
+import uk.gov.nationalarchives.tdr.transfer.service.Config.Configuration
 
 object TransferServiceServer extends IOApp {
+  private val config = ConfigSource.default.load[Configuration] match {
+    case Left(value)  => throw new RuntimeException(s"Failed to load database migration config${value.prettyPrint()}")
+    case Right(value) => value
+  }
+  private val apiPort: Port = Port.fromInt(config.api.port).getOrElse(port"8080")
 
   val healthCheckRoute: HttpRoutes[IO] = HttpRoutes.of[IO] { case GET -> Root / "healthcheck" =>
     Ok("Healthy")
@@ -24,7 +32,7 @@ object TransferServiceServer extends IOApp {
   private val transferServiceServer = EmberServerBuilder
     .default[IO]
     .withHost(ipv4"0.0.0.0")
-    .withPort(port"8080")
+    .withPort(apiPort)
     .withHttpApp(finalApp)
     .build
 
