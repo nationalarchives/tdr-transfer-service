5c049739eb7be0384a8cdb4ac8d8d47ddc301c0a
TomJKing <tom.king@nationalarchives.gov.uk>
2024-09-19 08:39:31 +0100

    Add logging to services

--- src/test/scala/uk/gov/nationalarchives/tdr/transfer/service/BaseSpec.scala
+++ src/test/scala/uk/gov/nationalarchives/tdr/transfer/service/BaseSpec.scala
@@ -1,9 +1,12 @@
 package uk.gov.nationalarchives.tdr.transfer.service
 
+import cats.effect.IO
 import org.mockito.MockitoSugar
 import org.scalatest.EitherValues
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import org.typelevel.log4cats.SelfAwareStructuredLogger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
 import sttp.client3.{HttpURLConnectionBackend, Identity, SttpBackend}
 import uk.gov.nationalarchives.tdr.keycloak.TdrKeycloakDeployment
 import uk.gov.nationalarchives.tdr.transfer.service.api.model.LoadModel.{MetadataPropertyDetails, TransferConfiguration}
@@ -11,6 +14,7 @@ import uk.gov.nationalarchives.tdr.transfer.service.api.model.LoadModel.{Metadat
 import scala.concurrent.ExecutionContextExecutor
 
 trait BaseSpec extends AnyFlatSpec with MockitoSugar with Matchers with EitherValues {
+  implicit def logger: SelfAwareStructuredLogger[IO] = Slf4jLogger.getLogger[IO]
   implicit val executionContext: ExecutionContextExecutor = scala.concurrent.ExecutionContext.global
   implicit val backend: SttpBackend[Identity, Any] = HttpURLConnectionBackend()
   implicit val tdrKeycloakDeployment: TdrKeycloakDeployment = TdrKeycloakDeployment("authUrl", "realm", 60)
