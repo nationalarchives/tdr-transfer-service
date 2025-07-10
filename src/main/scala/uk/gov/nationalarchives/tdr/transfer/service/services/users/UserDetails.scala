2afd5de0554bc9ac78115b443d47fc46fa6ae567
TomJKing <tom.king@nationalarchives.gov.uk>
2025-07-10 14:10:32 +0100

    Service to retrieve user details
    Support for send emails to users once SharePoint upload has completed

---
+++ src/main/scala/uk/gov/nationalarchives/tdr/transfer/service/services/users/UserDetails.scala
@@ -0,0 +1,45 @@
+package uk.gov.nationalarchives.tdr.transfer.service.services.users
+
+import org.keycloak.OAuth2Constants
+import org.keycloak.admin.client.resource.{RealmResource, UsersResource}
+import org.keycloak.admin.client.{Keycloak, KeycloakBuilder}
+import org.keycloak.representations.idm.UserRepresentation
+import sttp.client3.{HttpURLConnectionBackend, Identity, SttpBackend}
+import uk.gov.nationalarchives.tdr.transfer.service.ApplicationConfig
+
+class UserDetails(client: Keycloak, authConfig: ApplicationConfig.Auth)(implicit
+    backend: SttpBackend[Identity, Any]
+) {
+  private def realmResource(client: Keycloak): RealmResource = client.realm(authConfig.realm)
+  private def usersResource(realm: RealmResource): UsersResource = realm.users()
+
+  def getUserRepresentation(userId: String): UserRepresentation = {
+    val realm = realmResource(client)
+    val users = usersResource(realm)
+
+    try {
+      val userResource = users.get(userId)
+      userResource.toRepresentation
+    } catch {
+      case e: Exception => throw new RuntimeException(s"No valid user found $userId: ${e.getMessage}")
+    }
+  }
+
+}
+
+object UserDetails {
+  implicit val backend: SttpBackend[Identity, Any] = HttpURLConnectionBackend()
+  private val authConfig: ApplicationConfig.Auth = ApplicationConfig.appConfig.auth
+
+  def apply()(implicit backend: SttpBackend[Identity, Any]): UserDetails = {
+    val client = KeycloakBuilder
+      .builder()
+      .serverUrl(authConfig.url)
+      .realm(authConfig.realm)
+      .clientId(authConfig.clientId)
+      .clientSecret(authConfig.clientSecret)
+      .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
+      .build()
+    new UserDetails(client, authConfig)
+  }
+}
---
+++ src/test/scala/uk/gov/nationalarchives/tdr/transfer/service/services/users/UserDetailsSpec.scala
@@ -0,0 +1,29 @@
+package uk.gov.nationalarchives.tdr.transfer.service.services.users
+
+import uk.gov.nationalarchives.tdr.transfer.service.{ApplicationConfig, BaseSpec}
+
+class UserDetailsSpec extends BaseSpec {
+  private val authConfig = ApplicationConfig.Auth("http://localhost:9002/auth", "tdr", "auth-client-id", "auth-client-secret")
+  "'getUserDetails'" should "return the correct user details" in {
+    authOk
+    keycloakGetUser
+    val userDetails = new UserDetails(keycloakCreateAdminClient, authConfig)
+
+    val userRepresentation = userDetails.getUserRepresentation(keycloakUserId)
+    userRepresentation.getFirstName should be("FirstName")
+    userRepresentation.getLastName should be("LastName")
+    userRepresentation.getEmail should be("firstName.lastName@something.com")
+    userRepresentation.getId should be(keycloakUserId)
+  }
+
+  "'getUserDetails'" should "throw a run time exception if no user representation found" in {
+    authOk
+    val nonExistentUserId = "nonExistentUserId"
+    val userDetails = new UserDetails(keycloakCreateAdminClient, authConfig)
+
+    val exception = intercept[RuntimeException] {
+      userDetails.getUserRepresentation(nonExistentUserId)
+    }
+    exception.getMessage should equal(s"No valid user found $nonExistentUserId: HTTP 404 Not Found")
+  }
+}
