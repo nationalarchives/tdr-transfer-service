2baa27d4867be077c3f556bf10f3b0709d2dad13
TomJKing <tom.king@nationalarchives.gov.uk>
2025-07-16 09:02:55 +0100

    Name controller val correctly

--- src/main/scala/uk/gov/nationalarchives/tdr/transfer/service/api/TransferServiceServer.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/transfer/service/api/TransferServiceServer.scala
@@ -36,17 +36,17 @@ object TransferServiceServer extends IOApp {
 
   private val openApiInfo: Info = Info(infoTitle, infoVersion, description = infoDescription)
   private val loadController = LoadController()
-  private val processingController = TransferManagementController()
+  private val transferManagementController = TransferManagementController()
 
   private val documentationEndpoints =
-    SwaggerInterpreter().fromEndpoints[IO](loadController.endpoints ++ processingController.endpoints, openApiInfo)
+    SwaggerInterpreter().fromEndpoints[IO](loadController.endpoints ++ transferManagementController.endpoints, openApiInfo)
 
   val healthCheckRoute: HttpRoutes[IO] = HttpRoutes.of[IO] { case GET -> Root / "healthcheck" =>
     Ok("Healthy")
   }
 
   private val allRoutes =
-    Http4sServerInterpreter[IO]().toRoutes(documentationEndpoints) <+> loadController.routes <+> processingController.routes <+> healthCheckRoute
+    Http4sServerInterpreter[IO]().toRoutes(documentationEndpoints) <+> loadController.routes <+> transferManagementController.routes <+> healthCheckRoute
 
   private def throttleService(service: HttpApp[IO]): IO[HttpApp[IO]] = Throttle.httpApp[IO](
     amount = appConfig.transferServiceApi.throttleAmount,
