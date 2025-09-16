b5d862a6e32f8d264b90a57f4c8da862d2ade13d
TomJKing <tom.king@nationalarchives.gov.uk>
2025-09-16 13:25:18 +0100

    No longer handling transfer management via API

--- src/test/scala/uk/gov/nationalarchives/tdr/transfer/service/api/auth/TokenAuthenticatorSpec.scala
+++ src/test/scala/uk/gov/nationalarchives/tdr/transfer/service/api/auth/TokenAuthenticatorSpec.scala
@@ -2,7 +2,7 @@ package uk.gov.nationalarchives.tdr.transfer.service.api.auth
 
 import cats.effect.unsafe.implicits.global
 import uk.gov.nationalarchives.tdr.transfer.service.BaseSpec
-import uk.gov.nationalarchives.tdr.transfer.service.TestUtils.{invalidToken, transferServiceUserId, userId, validClientToken, validUserToken}
+import uk.gov.nationalarchives.tdr.transfer.service.TestUtils.{invalidToken, userId, validUserToken}
 
 class TokenAuthenticatorSpec extends BaseSpec {
   "'authenticateStandardUserToken'" should "return authenticated token when token valid" in {
@@ -22,34 +22,6 @@ class TokenAuthenticatorSpec extends BaseSpec {
   "'authenticateStandardUserToken'" should "return authentication error when token invalid" in {
     val token = invalidToken
     val response = TokenAuthenticator().authenticateStandardUserToken(token.token).unsafeRunSync()
-
-    response.left.toOption.get.message shouldBe "Invalid token issuer. Expected 'http://localhost:8000/auth/realms/tdr'"
-  }
-
-  "'authenticateClientToken'" should "return authenticated transfer service client token when token valid" in {
-    val validToken = validClientToken()
-    val response = TokenAuthenticator().authenticateClientToken(validToken.token).unsafeRunSync()
-
-    response.toOption.get.token.userId shouldBe transferServiceUserId
-  }
-
-  "'authenticateClientToken'" should "return authentication error when transfer service client does not have the 'data-load' role" in {
-    val validToken = validClientToken(role = "some-role")
-    val response = TokenAuthenticator().authenticateClientToken(validToken.token).unsafeRunSync()
-
-    response.left.toOption.get.message shouldBe s"$transferServiceUserId does not have correct authorisation"
-  }
-
-  "'authenticateClientToken'" should "return authentication error when client is not the transfer service client" in {
-    val validToken = validClientToken(clientId = "random-client-id")
-    val response = TokenAuthenticator().authenticateClientToken(validToken.token).unsafeRunSync()
-
-    response.left.toOption.get.message shouldBe s"$transferServiceUserId does not have correct authorisation"
-  }
-
-  "'authenticateClientToken'" should "return authentication error when token invalid" in {
-    val token = invalidToken
-    val response = TokenAuthenticator().authenticateClientToken(token.token).unsafeRunSync()
 
     response.left.toOption.get.message shouldBe "Invalid token issuer. Expected 'http://localhost:8000/auth/realms/tdr'"
   }
