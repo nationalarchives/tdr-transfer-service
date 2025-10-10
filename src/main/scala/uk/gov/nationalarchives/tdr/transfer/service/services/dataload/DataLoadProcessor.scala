2045b45f25bbd0ecf4e85ba59c05873d857ba432
Thanh Lam <thanh.lam@nationalarchives.gov.uk>
2025-10-10 10:11:25 +0100

    scalafmt

--- src/main/scala/uk/gov/nationalarchives/tdr/transfer/service/services/dataload/DataLoadProcessor.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/transfer/service/services/dataload/DataLoadProcessor.scala
@@ -24,7 +24,7 @@ class DataLoadProcessor(messageService: Messages, s3Config: ApplicationConfig.S3
 
     if (!clientSideErrors) {
       logger.info(s"Triggering aggregate processing for transfer: $transferId")
-      val eventMessage = AggregateProcessingEvent(event.source.toString, metadataSourceBucket, metadataSourceObjectPrefix, dataLoadErrors) //pass event source
+      val eventMessage = AggregateProcessingEvent(event.source.toString, metadataSourceBucket, metadataSourceObjectPrefix, dataLoadErrors)
       messageService.sendAggregateProcessingEventMessage(transferId, eventMessage)
     }
     IO(LoadCompletionResponse(transferId, !clientSideErrors && !dataLoadErrors))
