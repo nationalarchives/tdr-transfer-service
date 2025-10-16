0bbbaacd93d39f442642052d3ac979986a32602c
TomJKing <tom.king@nationalarchives.gov.uk>
2025-10-16 08:28:35 +0100

    Add source system to consignment metadata

--- src/main/scala/uk/gov/nationalarchives/tdr/transfer/service/services/GraphQlApiService.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/transfer/service/services/GraphQlApiService.scala
@@ -2,20 +2,27 @@ package uk.gov.nationalarchives.tdr.transfer.service.services
 
 import cats.effect.IO
 import cats.implicits.catsSyntaxOptionId
+import graphql.codegen.AddOrUpdateConsignmenetMetadata
+import graphql.codegen.AddOrUpdateConsignmenetMetadata.{addOrUpdateConsignmentMetadata => acm}
 import graphql.codegen.AddConsignment
 import graphql.codegen.AddConsignment.{addConsignment => ac}
 import graphql.codegen.StartUpload.{startUpload => su}
-import graphql.codegen.types.{AddConsignmentInput, StartUploadInput}
+import graphql.codegen.types.{AddConsignmentInput, AddOrUpdateConsignmentMetadata, AddOrUpdateConsignmentMetadataInput, StartUploadInput}
 import sttp.client3.{HttpURLConnectionBackend, Identity, SttpBackend}
 import uk.gov.nationalarchives.tdr.GraphQLClient
 import uk.gov.nationalarchives.tdr.keycloak.Token
 import uk.gov.nationalarchives.tdr.transfer.service.ApplicationConfig.appConfig
+import uk.gov.nationalarchives.tdr.transfer.service.api.model.SourceSystem.SourceSystemEnum.SourceSystem
 
 import java.util.UUID
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-class GraphQlApiService(addConsignmentClient: GraphQLClient[ac.Data, ac.Variables], startUploadClient: GraphQLClient[su.Data, su.Variables])(implicit
+class GraphQlApiService(
+    addConsignmentClient: GraphQLClient[ac.Data, ac.Variables],
+    consignmentMetadataClient: GraphQLClient[acm.Data, acm.Variables],
+    startUploadClient: GraphQLClient[su.Data, su.Variables]
+)(implicit
     backend: SttpBackend[Identity, Any]
 ) {
 
@@ -23,11 +30,17 @@ class GraphQlApiService(addConsignmentClient: GraphQLClient[ac.Data, ac.Variable
     def toIO: IO[T] = IO.fromFuture(IO(f))
   }
 
-  def addConsignment(token: Token): IO[AddConsignment.addConsignment.AddConsignment] = {
+  def addConsignment(token: Token, sourceSystem: SourceSystem): IO[AddConsignment.addConsignment.AddConsignment] = {
     for {
-      result <- addConsignmentClient.getResult(token.bearerAccessToken, ac.document, ac.Variables(AddConsignmentInput(None, "standard")).some).toIO
-      data <- IO.fromOption(result.data)(new RuntimeException(s"Consignment not added for user ${token.userId}"))
-    } yield data.addConsignment
+      addConsignmentResult <- addConsignmentClient.getResult(token.bearerAccessToken, ac.document, ac.Variables(AddConsignmentInput(None, "standard")).some).toIO
+      addConsignmentData <- IO.fromOption(addConsignmentResult.data)(new RuntimeException(s"Consignment not added for user ${token.userId}"))
+      consignmentId = addConsignmentData.addConsignment.consignmentid.get
+      consignmentMetadata = AddOrUpdateConsignmentMetadata("SourceSystem", sourceSystem.toString)
+      consignmentMetadataResult <- consignmentMetadataClient
+        .getResult(token.bearerAccessToken, acm.document, acm.Variables(AddOrUpdateConsignmentMetadataInput(consignmentId, List(consignmentMetadata))).some)
+        .toIO
+      _ <- IO.fromOption(consignmentMetadataResult.data)(new RuntimeException(s"Consignment metadata not added for $consignmentId"))
+    } yield addConsignmentData.addConsignment
   }
 
   def startUpload(token: Token, consignmentId: UUID, parentFolder: Option[String] = None): IO[String] = {
@@ -44,11 +57,13 @@ object GraphQlApiService {
 
   val service: GraphQlApiService = GraphQlApiService.apply(
     new GraphQLClient[ac.Data, ac.Variables](apiUrl),
+    new GraphQLClient[acm.Data, acm.Variables](apiUrl),
     new GraphQLClient[su.Data, su.Variables](apiUrl)
   )
 
   def apply(
       addConsignmentClient: GraphQLClient[ac.Data, ac.Variables],
+      consignmentMetadataClient: GraphQLClient[acm.Data, acm.Variables],
       startUploadClient: GraphQLClient[su.Data, su.Variables]
-  )(implicit backend: SttpBackend[Identity, Any]) = new GraphQlApiService(addConsignmentClient, startUploadClient)
+  )(implicit backend: SttpBackend[Identity, Any]) = new GraphQlApiService(addConsignmentClient, consignmentMetadataClient, startUploadClient)
 }
